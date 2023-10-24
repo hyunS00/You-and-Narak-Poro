037ed5bb304613727f21e7f3cdb40b7e53180551
hyunS00 <optifun72@gmail.com>
2023-10-24 23:27:37 +0900

    made by 추가

--- commands/Gernal/madeBy.js
+++ commands/Gernal/madeBy.js
@@ -1,12 +1,12 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('test').setDescription('Slash command handler test'),
+    data: new SlashCommandBuilder().setName('만든사람').setDescription('누가 만들었을까요??'),
     /**
      *
      * @param {import("discord.js").CommandInteraction} interaction
      */
     async execute(interaction) {
-        await interaction.reply({ content: `**Perfect!!**` });
+        await interaction.reply({ content: `made by 전산분대장 🤭` });
     },
 };
