62cc46fe40fee5a93894fb7d9fc6c54c0984f5ee
hyunS00 <optifun72@gmail.com>
2023-10-26 23:30:36 +0900

    포로야 메시지 이벤트 추가

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,8 +2,8 @@ module.exports = {
     name: 'messageCreate',
     once: false,
     execute(message) {
-        if (message.content == '안녕') {
-            message.reply({ content: `**반갑습니다!**` });
+        if (message.content == '포로야') {
+            message.reply({ content: `핫 둘 셋 넷 😄` });
         }
     },
 };
