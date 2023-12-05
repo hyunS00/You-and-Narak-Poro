2763aaa28f7a0be47c53d926897c3d4849107de0
hyunS00 <optifun72@gmail.com>
2023-12-05 23:36:13 +0900

    잔액확인 본인외 타유저 확인가능

--- commands/Gambling/checkMoney.js
+++ commands/Gambling/checkMoney.js
@@ -3,14 +3,29 @@ const gambling_Schema = require('../../models/gambling');
 const { EmbedBuilder } = require('@discordjs/builders');
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('도박잔액확인').setDescription('내 잔액을 확인합니다'),
+    data: new SlashCommandBuilder()
+        .setName('도박잔액확인')
+        .setDescription('내 잔액을 확인합니다')
+        .addUserOption((option) =>
+            option
+                .setName('확인할유저')
+                .setDescription('잔액 확인할 유저를 선택합니다')
+                .setRequired(true)
+        ),
 
     /**
      *
      * @param {import("discord.js").CommandInteraction} interaction
      */
     async execute(interaction) {
-        const gambling_find = await gambling_Schema.findOne({ userid: interaction.user.id });
+        const option_user = interaction.options.getUser('확인할유저');
+        let userid;
+        if (option_user) {
+            userid = option_user.id;
+        } else {
+            userid = interaction.user.id;
+        }
+        const gambling_find = await gambling_Schema.findOne({ userid });
 
         if (!gambling_find) {
             interaction.reply({ content: `데이터가 존재하지 않습니다` });
